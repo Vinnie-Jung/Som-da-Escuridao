91e901dfa6240418ec1e9bd8e1fe5fc69edcd371
Vinicius Jung <viniciusjung@outlook.com>
2023-10-02 06:47:20 -0300

    [PLAYER-MOVEMENT] - Adicionado sistema simples de animação para o jogador

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,11 +6,27 @@ const _SPEED : float = 10000.0
 
 func _physics_process(delta : float) -> void:
 	_movement(delta)
+	_animation()
 	move_and_slide()
 
 func _movement(delta : float) -> void:
 	# Verifies player direction
 	_direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
 	_direction = _direction.normalized() # Diagonals don't duplicate player's speed
-		
+	
 	self.velocity = _direction * (_SPEED * delta)
+
+
+func _animation() -> void:
+	# Animation tree (temporary)
+	if (_direction.y == -1):
+		$Animation.play("running_up")
+	elif (_direction.y == 1):
+		$Animation.play("running_down")
+	elif (_direction.x != 0):
+		$Animation.play("running_side")
+		$Animation.flip_h = false
+		if (_direction.x < 0):
+			$Animation.flip_h = true
+	else:
+		$Animation.play("idle")
