05d061053d9a6852ce3df9a2f40b7706d5f72de0
Vinicius Jung <viniciusjung@outlook.com>
2023-10-03 18:19:54 -0300

    [PLAYER-MOVEMENT] - Efetuando alguns ajustes no script do jogador

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,23 +2,19 @@ extends CharacterBody2D
 
 @onready var _direction : Vector2 = Vector2.ZERO
 @onready var _sprinting : bool = false
-@onready var _sprintSpeed : float = 0.0
+@onready var _sprintSpeed : float
 
-@onready var _startPosition = self.transform.get_origin()
+const _SPEED : float = 50.0
 
-const _SPEED : float = 5000.0
-
-func _process(delta):
+func _process(delta : float) -> void:
 	_sprintSpeed = 2.0 if (_sprinting) else 1.0
 	
-	# Key listeners
-	if (Input.is_action_pressed("Sprint")):
-		_sprinting = true
-	else:
-		_sprinting = false
+	_key_listeners()
 
 
 func _physics_process(delta : float) -> void:
+	delta *= 100
+	
 	_movement(delta)
 	_animation()
 	move_and_slide()
@@ -34,7 +30,7 @@ func _movement(delta : float) -> void:
 
 func _animation() -> void:
 	# Animation tree (temporary)
-	if (_sprinting):
+	if (_sprinting && _direction != Vector2.ZERO):
 		$animation.play("sprint")
 	elif (_direction.y == -1):
 		$animation.play("running_up")
@@ -45,3 +41,12 @@ func _animation() -> void:
 		$animation.flip_h = false if (_direction.x > 0) else true
 	else:
 		$animation.play("idle")
+
+
+func _key_listeners() -> void:
+	# Sprint (Shift key)
+	if (Input.is_action_pressed("Sprint")):
+		_sprinting = true
+	else:
+		_sprinting = false
+	pass
